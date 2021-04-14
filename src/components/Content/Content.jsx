e37250d3f61f26d1b7d336ae08a8dca43865fd05
Кирилл <samsuu@ya.ru>
2021-04-14 12:44:03 +0700

    Initial commit:

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MainTable from '../MainTable';
+
+function Content() {
+  return (
+    <>
+      <h1>Product Table</h1>
+      <MainTable />
+    </>
+  );
+}
+
+export default Content;
---
+++ src/components/MainTable/MainTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainTable() {
+  return <div>Table</div>;
+}
+
+export default MainTable;
